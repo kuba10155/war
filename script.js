d5d90617bc88c7a8bbd6355c4cd3cc6c25daf800
kuba10155 <j.zborowski24@gmail.com>
2023-01-28 03:20:17 +0100

    updated

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let deckId = ""
+let computerScore = 0
+let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const drawCardsBtn = document.getElementById("draw-cards-btn")
 const newDeckBtn = document.getElementById("new-deck-btn")
-const winnerText = document.getElementById("winner")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
 function handleClick() {
+  clear()
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
       deckId = data.deck_id
       drawCardsBtn.disabled = false
+      getRemainingCards(data.remaining)
     })
 }
 
@@ -17,15 +24,23 @@ function getCards() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-      let i=0
-      for(const child of cardsContainer.children) {
-        child.innerHTML = `
-          <img class="card" src="${data.cards[i].image}"></img>
-        `
-        i++
+      if(data.remaining > 0){
+        let i=0
+        for(const child of cardsContainer.children) {
+          child.innerHTML = `
+            <img class="card" src="${data.cards[i].image}"></img>
+          `
+          i++
+        }
+        getRemainingCards(data.remaining)
+        header.textContent = determineCardWinner(data.cards[0].value,
+        data.cards[1].value)
       }
-      winnerText.textContent = determineCardWinner(data.cards[0].value,
-      data.cards[1].value)
+      if(data.remaining == 0) {
+        drawCardsBtn.disabled = true
+        determineFinalWinner()
+      }
+      getScore()
     })
 }
 
@@ -35,9 +50,45 @@ function determineCardWinner(card1, card2) {
   const card1ValueIndex = cardsValue.indexOf(card1)
   const card2ValueIndex = cardsValue.indexOf(card2)
 
-  return card1ValueIndex > card2ValueIndex ?
-  "Card 1 wins!" : card2ValueIndex > card1ValueIndex ?
-  "Card 2 wins!" : "War!"
+  if(card1ValueIndex > card2ValueIndex) {
+    computerScore++
+    return "Computer wins!"
+  } else if(card1ValueIndex < card2ValueIndex) {
+    myScore++
+    return "You win!"
+  } else {
+    return "War!"
+  }
+  getScore()
+}
+
+function getRemainingCards(remaining) {
+  remainingText.textContent = `Remaining cards: ${remaining}`
+}
+
+function getScore() {
+  computerScoreEl.textContent = `Computer score: ${computerScore}`
+  myScoreEl.textContent = `My score: ${myScore}`
+}
+
+function clear() {
+  const a = document.getElementsByClassName("card-slot")
+  a[0].innerHTML = ""
+  a[1].innerHTML = ""
+  computerScore = 0
+  myScore = 0
+  getScore()
+  header.textContent = "Game of War"
+}
+
+function determineFinalWinner() {
+  if(computerScore > myScore) {
+    header.textContent = "The computer won the game!"
+  } else if(computerScore < myScore) {
+    header.textContent = "You won the game!"
+  } else {
+    header.textContent = "It's a tie game!"
+  }
 }
 
 newDeckBtn.addEventListener("click", handleClick)
